996c35d954384d112e5cb98ac6c306f49ede022d
ttavera234 <ttavera234@gmail.com>
2025-10-30 12:24:30 -0400

    Created User table in our database with SQLModel for handling logins.

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,7 @@
-print("Models!")
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
+    hashed_password: str
