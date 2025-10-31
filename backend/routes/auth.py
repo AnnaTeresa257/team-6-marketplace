b5b54d7adb3b75209b9de673e50947d3346219c7
ttavera234 <ttavera234@gmail.com>
2025-10-31 08:30:03 -0400

    Added some comments in auth.py to explain the code logic

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -3,23 +3,25 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session, select
 from datetime import timedelta
 
-from models import User, UserCreate, UserPublic, Token
-from security import (
+from ..models import User, UserCreate, UserPublic, Token
+from ..database import get_session
+from ..security import (
     verify_password,
     get_password_hash,
     create_access_token,
     ACCESS_TOKEN_EXPIRE_MINUTES
 )
 
-from database import get_session
-
+# Groups API endpoints into a modular, manageable components.
+# Allows group of related routes to be defined separately from the main application file
 auth_router = APIRouter(tags=["authentication"])
+
 
 @auth_router.post("/signup", response_model=UserPublic)
 def signup(user_data: UserCreate, session: Session = Depends(get_session)):
     user_exists = session.exec(
         select(User).where((User.email == user_data.email) | (User.username == user_data.username))
-    ).first()
+    ).first()  # Queries the database with a SQL wrapper (ORM) to check if the user already exists
 
     if user_exists:
         raise HTTPException(
@@ -27,25 +29,31 @@ def signup(user_data: UserCreate, session: Session = Depends(get_session)):
             detail="Username or email already registered"
         )
 
+    # User does not already exist, so hash their password and add the user's data into the database
     hashed_pwd = get_password_hash(user_data.password)
     new_user = User(
         username=user_data.username,
         email=user_data.email,
         hashed_password=hashed_pwd
     )
+
     session.add(new_user)
     session.commit()
     session.refresh(new_user)
+
+    # Validates and converts object into the UserPublic Pydantic model, effectively stripping hashed_password
     return new_user
 
 @auth_router.post("/login", response_model=Token)
 def login(
+        # Pre-built Pydantic model that tells FastAPI to look for data sent in the requested body as standard HTML form data (application/x-www-form-urlencoded)
+        # Specifically looks for username and password fields and parses the data
         form_data: OAuth2PasswordRequestForm = Depends(),
         session: Session = Depends(get_session)
 ):
     user = session.exec(
         select(User).where(User.username == form_data.username)
-    ).first()
+    ).first()  # Queries the database for the user with the given username
 
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
@@ -54,8 +62,10 @@ def login(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+    # User is valid, so generate JSON Web Token to authorize them
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         subject=user.username, expires_delta=access_token_expires
     )
+
     return {"access_token": access_token, "token_type": "bearer"}
