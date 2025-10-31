8fb1348b456b48abf0a6e7a059634a6792a14146
ttavera234 <ttavera234@gmail.com>
2025-10-31 08:58:18 -0400

    Edited dependencies.py's import statements and commented to explain some of its logic.

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,1 +1,37 @@
-print("dependencies")
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from sqlmodel import Session, select
+import jwt
+
+from database import get_session
+from models import User
+from security import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")  # Tells FastAPI that the frontend website can get a token by sending a POST request to /login
+
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        session: Session = Depends(get_session())
+) -> User:
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        # Extracts, decodes, and validates "Bearer" token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+
+    user = session.exec(select(User).where(User.username == username)).first()
+    if user is None:
+        raise credentials_exception
+    return user
