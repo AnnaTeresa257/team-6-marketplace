5bb5e94885ee53b86a9c9d34929d6d012ea7bb79
Alex-Ospina <alex.ospina333@gmail.com>
2025-10-31 01:04:42 -0400

    Added functionality to auth.py and security.py.

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,1 +1,61 @@
-print("login and signup routes")
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session, select
+from datetime import timedelta
+
+from models import User, UserCreate, UserPublic, Token
+from security import (
+    verify_password,
+    get_password_hash,
+    create_access_token,
+    ACCESS_TOKEN_EXPIRE_MINUTES
+)
+
+from database import get_session
+
+auth_router = APIRouter(tags=["authentication"])
+
+@auth_router.post("/signup", response_model=UserPublic)
+def signup(user_data: UserCreate, session: Session = Depends(get_session)):
+    user_exists = session.exec(
+        select(User).where((User.email == user_data.email) | (User.username == user_data.username))
+    ).first()
+
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username or email already registered"
+        )
+
+    hashed_pwd = get_password_hash(user_data.password)
+    new_user = User(
+        username=user_data.username,
+        email=user_data.email,
+        hashed_password=hashed_pwd
+    )
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
+
+@auth_router.post("/login", response_model=Token)
+def login(
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        session: Session = Depends(get_session)
+):
+    user = session.exec(
+        select(User).where(User.username == form_data.username)
+    ).first()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        subject=user.username, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
