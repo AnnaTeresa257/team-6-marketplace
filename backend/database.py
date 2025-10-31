85d3e066e2eadc57071073228cd0880f8defb3ed
ttavera234 <ttavera234@gmail.com>
2025-10-31 07:41:49 -0400

    Added database.py functionality, which sets up postgresql session for the application and stores the DB_URL safely in .env file.

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Session, create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Gets our DATABASE_URL without leaking private data
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:  # Sanity check to ensure DB URL was found
+    raise ValueError("DATABASE_URL was not found, verify .env file.")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def initialize_db():
+    SQLModel.metadata.create_all(engine)  # Creates tables in the database based on "table=True" flag
+
+# Ensures a new session is created for each request and closed when the request is done
+def get_session():
+    with Session(engine) as session:
+        yield session  # With yield --> Transaction safe session to the database, allows code to run and closes db at the end even if there is an error
